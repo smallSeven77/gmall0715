3346805f4d3ebffcb349b9dc59aff5067d48b3c9
谢鹏 <975711794@qq.com>
2019-12-25 21:24:02 +0800

    1225

---
+++ gmall-user-manage/src/main/java/com/atguigu/gmall0715/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.atguigu.gmall0715.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall0715.user.mapper.UserAddressMapper;
+import com.atguigu.gmall0715.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired(required = false)
+    private UserInfoMapper userInfoMapper;
+    @Autowired(required = false)
+    private UserAddressMapper userAddressMapper;
+
+    @Override
+    public List<UserInfo> findAll() {
+        return userInfoMapper.selectAll();
+    }
+
+    @Override
+    public List<UserAddress> findUserAddressByUserId(String userId) {
+        UserAddress userAddress = new UserAddress();
+        userAddress.setId(userId);
+        return userAddressMapper.select(userAddress);
+    }
+}
